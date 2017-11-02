3c4429e95f07d2a81cc68db87a91c433a00518ad
Alex Brazhenko <alex.brazhenko@xealth.io>
2017-11-02 13:29:33 -0700

    Update Partner sample code with pre-order configuration endpoint samples

---
+++ partner-api/src/commands/demo/preorderForms.js
@@ -0,0 +1,60 @@
+/** @flow */
+import PartnerApi from '~/partnerApi'
+import loadConfig from '~/config'
+
+const FORM_ID = 'registration_form_1'
+const REGISTRATION_FORM = {
+  'title': 'Registration form',
+  'type': 'object',
+  'required': [
+    'firstName'
+  ],
+  'properties': {
+    'lastName': {
+      'type': 'string',
+      'title': 'Last name'
+    },
+    'firstName': {
+      'type': 'string',
+      'title': 'First name'
+    },
+    'age': {
+      'type': 'integer',
+      'title': 'Age'
+    }
+  }
+}
+const ADD_FORM_BODY = {
+  formSchema: REGISTRATION_FORM,
+  uiSchema: {}
+}
+
+function demo(api: PartnerApi) {
+  return api.putPreorderForm(FORM_ID, ADD_FORM_BODY)
+  .then(() => {
+    return api.getPreorderForm(FORM_ID)
+  }).then(form => {
+    console.log(form.body)
+    return api.deletePreorderForm(FORM_ID)
+  }).then(() => {
+    return api.getPreorderForms()
+  }).then(forms => {
+    console.log(forms.body)
+  })
+}
+
+function main(argv: Object) {
+  const config = loadConfig(argv)
+  const api = new PartnerApi(config.partnerId, config.keyInfo, {baseUrl: config.serverBase, verbose: argv.verbose})
+  return demo(api)
+}
+
+exports.command = 'forms'
+exports.describe = 'Creates and then deletes a preorder form'
+exports.handler = function(argv: Object) {
+  return Promise.resolve().then(function() {
+    return main(argv)
+  }).catch(function(err) {
+    console.error('Oops:\n', err)
+  });
+}
